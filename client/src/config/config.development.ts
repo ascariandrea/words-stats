7a101807dba8698653fd73ebb37db914b9c88e17
Andrea Ascari <dev.ascariandrea@gmail.com>
2018-05-10 16:40:42 +0200

    Created config file

---
+++ client/src/config/config.development.ts
@@ -0,0 +1,7 @@
+import { IConfig } from "./config";
+
+const config: IConfig = {
+  baseURL: 'http://localhost:8080'
+}
+
+export default config;
---
+++ client/src/config/index.ts
@@ -0,0 +1,3 @@
+import config from './config.development';
+
+export default config;
