4cf57b60fa9f88dbbec13f2953a42fa3e28ce2a2
Andrea Ascari <dev.ascariandrea@gmail.com>
2018-05-11 13:42:15 +0200

    Added test to check correct web server response

--- api/src/test/scala/file/stats/FileStatsSpec.scala
+++ api/src/test/scala/file/stats/FileStatsSpec.scala
@@ -1,19 +1,57 @@
 package file.stats
 
+import akka.http.scaladsl.model._
 import org.scalatest.{Assertions, AsyncFunSuite, Matchers}
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import file.stats.models.WordStats
 
 class FileStatsSpec
-  extends AsyncFunSuite
+    extends AsyncFunSuite
     with Matchers
-    with Assertions {
-
-  test("count all the words in a text") {
-    FileStats.totalWordCount("hello world") shouldBe 2
-  }
+    with Assertions
+    with ScalatestRouteTest {
 
   test("counts of each occurrence of a word") {
-    val wordsCount = FileStats.countWords("hello world, hello world, world")
+    val map = List(("hello", 2), ("world", 2)).toMap
+    val wordsCount = FileStats.countWords(map, "World")
     wordsCount("hello") shouldBe 2
     wordsCount("world") shouldBe 3
   }
+
+  test("Should parse the file and return a proper json object") {
+
+    val multipartForm =
+      Multipart.FormData(
+        Multipart.FormData.BodyPart.Strict(
+          "doc",
+          HttpEntity(
+            ContentTypes.`text/plain(UTF-8)`,
+            """
+            |this is the first line
+            |this is the second line
+            |this is the third line
+          """.stripMargin
+          ),
+          Map("filename" -> "doc.txt")
+        ))
+
+    Post("/fileStats", multipartForm) ~> WebServer.routes ~> check {
+
+      import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
+      import io.circe.generic.auto._
+
+      status shouldEqual StatusCodes.Created
+      responseAs[WordStats].totalWords shouldBe 15
+      responseAs[WordStats].words shouldBe Map(
+        "this" -> 3,
+        "is" -> 3,
+        "line" -> 3,
+        "second" -> 1,
+        "third" -> 1,
+        "first" -> 1,
+        "the" -> 3
+      )
+    }
+  }
+
 }
