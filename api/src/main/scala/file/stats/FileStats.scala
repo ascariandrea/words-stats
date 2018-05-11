62dd875da87e79ade3487afb0d6895adec9f2348
Andrea Ascari <dev.ascariandrea@gmail.com>
2018-05-11 13:40:26 +0200

    Put together the logic ina single method to extract stats from receive string

--- api/src/main/scala/file/stats/FileStats.scala
+++ api/src/main/scala/file/stats/FileStats.scala
@@ -1,11 +1,34 @@
 package file.stats
 
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Framing, Source}
+import akka.util.ByteString
+import file.stats.models.WordStats
+import io.circe.Json
+import scala.concurrent.{ExecutionContextExecutor, Future}
+import io.circe.syntax._
+
 object FileStats {
 
-  def countWords(text: String): Map[String, Int] =
-    text.split("\\W+")
-      .groupBy(s => s)
-      .map(g => (g._1, g._2.length))
+  implicit val system: ActorSystem = ActorSystem()
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
-  def totalWordCount(text: String): Int = text.split("\\W+").length
+  def countWords(map: Map[String, Int], word: String): Map[String, Int] =
+    map + (word.toLowerCase -> (map.getOrElse(word.toLowerCase, 0) + 1))
+
+  def getForFile(byteSource: Source[ByteString, Any]): Future[Json] = {
+
+    byteSource
+      .via(Framing.delimiter(ByteString("\n"), 1024, true))
+      .mapConcat(_.utf8String.toLowerCase().split("\\W+").filter(_.isEmpty == false).toVector)
+      .runFold((0, Map.empty[String, Int]))(
+        (acc, w) =>
+          (
+            acc._1 + 1,
+            countWords(acc._2, w)
+        ))
+      .map(stats => WordStats(stats._1, stats._2).asJson)
+  }
 }
